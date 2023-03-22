3467885afbb9fb96e3754adfedb167142e194f45
Bart Bas Elsenaar <advendelie@hotmail.com>
2023-03-22 11:18:02 +0100

    added comunication
    Its pretty empty though

---
+++ 3pi_app11/3pi_app11/Comunication.c
@@ -0,0 +1,6 @@
+/*
+ * comunication.c
+ *
+ * Created: 22/03/2023 11:10:47
+ *  Author: bbels
+ */ 
---
+++ 3pi_app11/3pi_app11/Comunication.h
@@ -0,0 +1,16 @@
+/*
+ * IncFile1.h
+ *
+ * Created: 22/03/2023 11:11:04
+ *  Author: bbels
+ */ 
+
+
+#ifndef COMUNICATION_H_
+#define COMUNICATION_H_
+
+
+
+
+
+#endif /* COMUNICATION_H_ */
