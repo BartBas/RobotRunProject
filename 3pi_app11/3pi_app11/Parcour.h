c6c5163cc5eaaa54a76fd458c51727bd3e77c7ce
joostjoostjoost <85460261+joostjoostjoost@users.noreply.github.com>
2023-03-22 11:15:46 +0100

    blub

--- 3pi_app11/3pi_app11/Parcour.h
+++ 3pi_app11/3pi_app11/Parcour.h
@@ -2,15 +2,30 @@
  * Parcour.h
  *
  * Created: 22/03/2023 11:02:37
- *  Author: joren
+ *  Author: Joost
  */ 
 
 
 #ifndef PARCOUR_H_
 #define PARCOUR_H_
 
+typedef enum {					// enum for the corners and junctions
+	Straight,					// ?
+	Line_end,					//
+	Barcode,					// ?
+	
+	Left_corner,				// ?
+	Right_corner,				// ?
+	
+	Straight_right_junction,    // ?
+	Straight_left_junction,		// ?
+	T_junction,					// ?
+	
+	X_junction					// ?
+	
+} Junctions;
 
-void parcour();
 
+void parcour(void);
 
 #endif /* PARCOUR_H_ */
--- 3pi_app11/3pi_app11/parcour.c
+++ 3pi_app11/3pi_app11/parcour.c
@@ -5,65 +5,49 @@
  *  Author: joren
  */ 
 
-#include "parcour.h"
 #include "movementManagement.h"
 
 #include <pololu/3pi.h>
 #include <avr/pgmspace.h>
 
+char route[20];
 
 Junctions situatie = Straight;
 
-
-void parcour() {
+void parcour(void) {
 	while(1) {
 		situatie = direction();
 		
 		switch (situatie) {
 			case Barcode: // end of parcour
-			clear();
-			lcd_goto_xy(0, 0);
-			print("bar code");
 			set_motors(0,0);
-			delay(1000);
-			break;
+			return;
 			
 			case X_junction:
 			case Left_corner:
 			case T_junction:
 			case Straight_left_junction:
-			clear();
-			lcd_goto_xy(0, 0);
-			print("L");
-			motorControl('L');
-			clear();
+			motorcontrol('L');
+			route += 'L';
 			break;
 			
 			case Right_corner:
-			clear();
-			lcd_goto_xy(0, 0);
-			print("R");
-			motorControl('R');
-			clear();
+			motorcontrol('R');
+			route += 'R';
 			break;
 			
 			case Straight_right_junction:
+			route += 'S';
 			case Straight:
-			clear();
-			lcd_goto_xy(0, 0);
-			print("S");
-			motorControl('S');
-			clear();
+			motorcontrol('S');
 			break;
 			
 			case Line_end:
-			clear();
-			lcd_goto_xy(0, 0);
-			print("End");
-			motorControl('T');
-			clear();
+			motorcontrol('T');
+			route += 'T';
 			break;
-			
 		}
+		
+		//if  route[-2]
 	}
 }
