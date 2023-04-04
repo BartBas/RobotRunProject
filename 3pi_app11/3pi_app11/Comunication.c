0ef13ce9eb2cae349d3ed4fc97fecae4cc95e3ef
Bart Bas Elsenaar <advendelie@hotmail.com>
2023-04-04 05:50:10 +0200

    added clean version of Communications

--- 3pi_app11/3pi_app11/Comunication.c
+++ 3pi_app11/3pi_app11/Comunication.c
@@ -5,16 +5,148 @@
  *  Author: bbelsenaar
  */ 
 #include "Comunication.h"
+#include <pololu/3pi.h>
+#include <avr/pgmspace.h>
 
-int Send(Communications *self){
-	return 1;
+const char welcomed[] PROGMEM = ">g32>>c32";
+
+const char fugue[] PROGMEM =
+"! O5 L16 agafaea dac+adaea fa<aa<bac#a dac#adaea f"
+"O6 dcd<b-d<ad<g d<f+d<gd<ad<b- d<dd<ed<f+d<g d<f+d<gd<ad"
+"L8 MS <b-d<b-d MLe-<ge-<g MSc<ac<a ML d<fd<f O5 MS b-gb-g"
+"ML >c#e>c#e MS afaf ML gc#gc# MS fdfd ML e<b-e<b-"
+"O6 L16ragafaea dac#adaea fa<aa<bac#a dac#adaea faeadaca"
+"<b-acadg<b-g egdgcg<b-g <ag<b-gcf<af dfcf<b-f<af"
+"<gf<af<b-e<ge c#e<b-e<ae<ge <fe<ge<ad<fd"
+"O5 e>ee>ef>df>d b->c#b->c#a>df>d e>ee>ef>df>d"
+"e>d>c#>db>d>c#b >c#agaegfe f O6 dc#dfdc#<b c#4";
+
+
+#define START 010
+#define STOP 101
+
+void filllocationarrays(Communications *self){
+	char arrx[26];
+	char arry[26];
+	char set;
+	int placement=0;
+	if (self->Recieved[1]==1){
+		arrx[placement]=0;
+		arry[placement]=0;
+		placement++;
+	}
+	for (int i=0;i<8;i++){
+		set = ((self->Recieved[2] >> i)  & 0x01);
+		if (i<5){
+			if (set == 1){
+			arrx[placement]=i+1;
+			arry[placement]=0;
+			placement++;}
+		}else{
+			if (set == 1){
+			arrx[placement]=i-5;
+			arry[placement]=1;
+			placement++;}
+		}
+	}
+		for (int i=0;i<8;i++){
+			set = ((self->Recieved[3] >> i)  & 0x01);
+			if (i==0){
+				if (set == 1){
+				arrx[placement]=i+4;
+				arry[placement]=1;
+				placement++;}
+			}else if(i<6){
+				if (set == 1){
+				arrx[placement]=i-1;
+				arry[placement]=2;
+				placement++;}
+			}else{
+				if (set == 1){
+				arrx[placement]=i-6;
+				arry[placement]=3;
+				placement++;}
+			}
+		}
+		for (int i=0;i<8;i++){
+			set = ((self->Recieved[4] >> i)  & 0x01);
+			if (i<3){
+				if (set == 1){
+				arrx[placement]=i+2;
+				arry[placement]=3;
+				placement++;}
+			}else{
+				if (set == 1){
+				arrx[placement]=i-3;
+				arry[placement]=4;
+				placement++;}
+			}
+		}
+	for (int i=0;i<=placement;i++){
+		self->Orderarx[i]=arrx[i];
+		self->Orderary[i]=arry[i];
+	}
 }
-int Recieve(Communications *self){
-	return 1;
+			
+
+
+void sendACK(Communications *self ){
+						char tmp[3];
+						tmp[0]=START;
+						tmp[1]=255;
+						tmp[2]=STOP;
+						serial_send_blocking(tmp,self->val);
 }
 
 void Update(Communications *self){
-	
+	char tmp =0;
+			if (serial_receive_buffer_full()==1){
+				if (self->Recieved[0]==8 && self->Recieved[self->val-1]==101){
+				tmp = 1;
+				//play_from_program_space(fugue);
+				
+				sendACK(self); // stops here to send acknowledge
+				if (self->Recieved[5]==254 || self->EmergencyStop==1){	//enter emergency stop
+									play_from_program_space(fugue);
+					self->EmergencyStop=1;
+				}
+				
+				if (self->Recieved[8]==250 && self->EmergencyStop==1){	// Leave emergency stop
+					self->EmergencyStop=0;
+				}
+				
+				if (self->Recieved[8]==245){							// Enter Spin Mode
+					self->EmergencyStop=3;
+				}
+				if (self->Recieved[8]==240){							// Enter Manual Mode
+					self->EmergencyStop=4;
+					for (int i = 0; i < 4; i++){
+						self->Direction[i]=self->Recieved[i+9];
+					}
+				}
+				
+				if (self->locationx==-1 && self->locationy==-1){
+					filllocationarrays(self);
+				}
+				}
+			}
+			
+				for (char i=0;i<self->val;i++){
+					self->msgBuffer[i]=i;
+				}
+				self->msgBuffer[0]=START;
+				self->msgBuffer[1]=self->batterylvl;
+				self->msgBuffer[2]=self->magprocess;
+				self->msgBuffer[3]=self->locationx;
+				self->msgBuffer[4]=self->locationy;
+				self->msgBuffer[5]=self->EmergencyStop;
+				self->msgBuffer[self->val-1]=STOP;
+				serial_send(self->msgBuffer, self->val);
+
+				delay_ms(10000); // test
+				if (tmp == 1){
+					serial_receive(self->Recieved,self->val);
+				}		
 }
 
 void fillarrays(Communications *self){
@@ -27,11 +159,24 @@ void fillarrays(Communications *self){
 		}
 }
 
-Communications COMS_INIT(){
-Communications myCom;
-	myCom.Send=Send;
-	myCom.Recieve=Recieve;
-	fillarrays(&myCom);
-	myCom.Update=Update;
-return myCom;	
+void Communications_INIT(Communications * myCom){
+	myCom->val = Max_Bytes;	
+
+
+	//Setup Communications
+	serial_set_baud_rate(9600);
+	serial_receive(myCom->Recieved,myCom->val);
+
+	//filling in functions
+	myCom->Update		 = Update;
+		
+	//filling in default values
+	myCom->EmergencyStop = 0;
+	myCom->batterylvl	 = 55;
+	myCom->magprocess	 = 20;
+	myCom->locationx	 = 4;
+	myCom->locationy	 = 5;
+	
+	
+	fillarrays(myCom);
 }
--- 3pi_app11/3pi_app11/Comunication.h
+++ 3pi_app11/3pi_app11/Comunication.h
@@ -9,36 +9,34 @@
 #ifndef COMUNICATION_H_
 #define COMUNICATION_H_
 
+#define Max_Bytes 26 // BYTES send and received per message
+
 typedef struct Communications {
 
 //PRIVATE	
 
-	void (*Close)(struct Communications *); 		// close handle
-	int (*Send)(struct Communications *); 			// send msg
-	int val;										// Lenght of array
-	char msgBuffer[26];								// Buffer for msg to be send
-	int SendSuccesfull;
-	int (*Recieve)(struct Communications *);
-	char Recieved[26];
-	int newmsg;
+	char val;												// Length of message buffer
+	char msgBuffer[Max_Bytes];								// Buffer for message to be send
+	char Recieved[Max_Bytes];								// Buffer for message to be received
 
 //PUBLIC
-
-	// LOCATIONS
-	char Orderary[25];
-	char Orderarx[25];
-		
-	// CHECK FOR UPDATES/MSGS
-	char EmergancyStop; // OUTPUT  0 for clear 1 for stop
-	char batterylvl;	// INPUT % of battery
-	char magprocess;	// INPUT % of order completion
-	char locationx;		// INPUT Location of robot x
-	char locationy;		// INPUT Location of robot y
-	void (*Update)(struct Communications *,int);
 	
-		
-
-
+	//	MANUAL controls
+	char Direction[4];	// OUTPUT	0 for button not pressed; 1 for button pressed. {UP, LEFT, DOWN, RIGHT}
+	
+	// LOCATIONS
+	char Orderary[25];	// OUTPUT	array of order locations for robot x
+	char Orderarx[25];	// OUTPUT	array of order locations for robot x
+	
+	// CHECK FOR UPDATES/MSGS
+	char EmergencyStop; // OUTPUT	0 for clear; 1 for stop; 2 for spin; 3 for manual
+	char batterylvl;	// INPUT	% of battery
+	char magprocess;	// INPUT	% of order completion
+	char locationx;		// INPUT	Location of robot x;  set to -1 for home -2 for order completion
+	char locationy;		// INPUT	Location of robot y;  set to -1 for home -2 for order completion
+	
+	void (*Update)(struct Communications *);
+	
 } Communications;
 
 void Communications_INIT(struct Communications*);
